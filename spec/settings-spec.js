015a508577aeaa7226b993239e244c427334ae7c
Daniel Brodin <daniel@itiden.se>
2016-10-24 10:29:04 +0200

    Fix weird merge
    Not sure what happened but hopefully everything is fixed

--- spec/settings-spec.js
+++ spec/settings-spec.js
@@ -1,5 +1,3 @@
-/* global describe it expect */
-
 const Settings = require('../lib/Settings');
 
 const settings = new Settings();
@@ -31,5 +29,24 @@ describe('Settings', () => {
 
     expect(atom.config.get('foo.bar')).toBe('baz');
     expect(atom.config.get('foo.bar', { scope: ['.js.source'] })).toBe('not-baz');
+  });
+
+  it('loads settings with duplicate keys', () => {
+    const config = {
+      foo: {
+        bar: {
+          baz: 'overwritten',
+          boo: 'not-duplicate',
+        },
+      },
+      'foo.bar': {
+        baz: 'duplicate',
+      },
+    };
+
+    settings.load(config);
+
+    expect(atom.config.get('foo.bar.boo')).toBe('not-duplicate');
+    expect(atom.config.get('foo.bar.baz')).toBe('duplicate');
   });
 });
